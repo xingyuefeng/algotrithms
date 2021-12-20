40c56cad1fc944eec35578955739899e4f4fa76e
xingyuefeng <15262870437@163.com>
2021-12-20 23:54:30 +0800

    feat: 增加插入排序

--- src/com/sort/Insertion.java
+++ src/com/sort/Insertion.java
@@ -1,5 +1,22 @@
 package com.sort;
 
 // 插入排序
-public class Insertion {
+public class Insertion extends SortTemp {
+
+    public void sort (int[] args) {
+        for (int i = 1; i < args.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (args[j] < args[j-1]) {
+                    exchange(args, args[j], args[j-1]);
+                }
+            }
+        }
+    }
+
+    public  void test(int [] args) {
+        sort(args);
+        assert isSorted(args);
+        show(args);
+    }
+
 }
--- src/com/sort/Main.java
+++ src/com/sort/Main.java
@@ -5,8 +5,12 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] ss) {
         // 选择排序
-        int[] args = {1,2,3,4,7,6};
-        SelectSort selectSort = new SelectSort();
-        selectSort.test(args);
+        int[] args = {1,2,3,4,7,6,5};
+//        SelectSort selectSort = new SelectSort();
+//        selectSort.test(args);
+
+        // 插入排序
+        SelectSort Insertion = new SelectSort();
+        Insertion.test(args);
     }
 }
